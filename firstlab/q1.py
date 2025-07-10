63afed3c4f5527637e9c7c5b562c01488d5c8144
sankeerthanreddypichili <sankeerthanreddypichili@gmail.com>
2025-07-10 22:33:37 +0530

    first question done

--- firstlab/q1.py
+++ firstlab/q1.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("hi")
+# Q1) program to count no of vowels and consonants in input string
+
+def count(inputstring):
+    vowels="aeiou"
+    inputstring=inputstring.lower()
+    v_count=0
+    c_count=0
+    for c in inputstring:
+        if c.isalpha():
+            if c in vowels:
+                v_count+=1
+            else:
+                c_count+=1
+    return v_count,c_count
+text="Machine_learning"
+print("vowels and consonats count is :",count(text))
